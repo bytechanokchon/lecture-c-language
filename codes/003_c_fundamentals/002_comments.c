12287fe36ae1c7b173fb3999f292f7e75f18a2af
Alphanumeric Str <mint.colorfuls@gmail.com>
2025-12-07 10:41:23 +0700

    lecture comment

---
+++ codes/003_c_fundamentals/002_comments.c
@@ -0,0 +1,13 @@
+/*
+    Author: Chanokchon Wongjumpa
+    Purpose: This program print out my name to the console
+    Date: 07/12/2025
+*/
+
+#include<stdio.h>
+
+int main(void) 
+{
+    printf("Hello, My name is Chanokchon"); // display my name
+    return 0;
+}
